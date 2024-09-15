84b5d356d213eb0916316674bcf791ba629ca3f9
Carlos Echeverria <c_echeverria.stefanini@skyairline.com>
2024-09-14 22:43:14 -0500

    chore: comment to fix deploy

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,11 +1,10 @@
-const getUserInfo = async() => {
-  const data = await fetch('https://api.example.com/sales')
-    .then(res => res.json());
-  return data; 
-};
+// const getUserInfo = async() => {
+//   const data = await fetch('https://api.example.com/sales')
+//     .then(res => res.json());
+//   return data; 
+// };
 
 export default function ProfileSection() {
-  const userInfo = getUserInfo();
   return (
     <>
       <h1>HELLO DASHBOARD PROFILE!</h1>
