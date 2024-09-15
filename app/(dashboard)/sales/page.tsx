64658ae44d072843c08df9665b53e458f12055a4
Carlos Echeverria <c_echeverria.stefanini@skyairline.com>
2024-09-14 22:29:41 -0500

    feat: auth user

--- app/(dashboard)/sales/page.tsx
+++ app/(dashboard)/sales/page.tsx
@@ -1,25 +1,53 @@
-"use client";
+'use client';
 
 import { useState, useEffect } from "react";
 import { CalendarIcon, PlusIcon } from "@primer/octicons-react";
-import { SalesMonthly } from "./interfaces/sales-monthly-response.dto";
+import { Sale, SalesMonthly } from "./interfaces/sales-monthly-response.dto";
 import { formatDate } from "../../../utils/date/FormatDate";
 
+// Fetch sales data from API
 const getSales = async (): Promise<SalesMonthly> => {
   const year = new Date().getFullYear();
   const month = new Date().getMonth() + 1;
   const yearMonth = `${year}-${month}`;
 
-  const data: SalesMonthly = await fetch(
+  const response = await fetch(
     `https://cloth-seller.vercel.app/api/v1/sell/get-by-month/${yearMonth}`,
     { cache: "no-store" }
-  ).then((res) => res.json());
-  return data;
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch sales data");
+  }
+  return response.json();
+};
+
+// Function to render table rows
+const renderTableRows = (sales: Sale[] | undefined) => {
+  return (sales ?? []).map((sale, index) => (
+    <tr key={index}>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">{sale.brand}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">{sale.description}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">{formatDate(sale.sellDate.toString())}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">{sale.user.name}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+        <p className="text-gray-600 whitespace-no-wrap">{sale.amount}</p>
+      </td>
+    </tr>
+  ));
 };
 
 export default function SalesSection() {
   const [sales, setSales] = useState<SalesMonthly | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
   const [filter, setFilter] = useState<string>("monthly");
 
   useEffect(() => {
@@ -28,30 +56,30 @@ export default function SalesSection() {
         const salesData = await getSales();
         setSales(salesData);
       } catch (error) {
-        console.error("Error fetching sales data:", error);
+        setError(error.message);
       } finally {
         setLoading(false);
       }
     };
-
     fetchSales();
   }, []);
 
-  const handleFilter = () => {
-    setFilter((prevFilter) =>
-      prevFilter === "monthly" ? "weekly" : "monthly"
-    );
+  const handleFilterToggle = () => {
+    setFilter((prevFilter) => (prevFilter === "monthly" ? "weekly" : "monthly"));
   };
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <p>Cargando información de las ventas del mes...</p>;
   }
 
-  if (!sales || (sales[filter] && sales[filter].length === 0)) {
+  if (error) {
+    return <p className="text-red-500">Error: {error}</p>;
+  }
+
+  if (sales?.monthly.length === 0) {
     return (
       <h2 className="uppercase text-gray-700">
-        No existen registros de ventas este{" "}
-        {filter === "monthly" ? "mes" : "semana"}...
+        No existen registros de ventas este mes...
       </h2>
     );
   }
@@ -59,26 +87,20 @@ export default function SalesSection() {
   return (
     <div className="container mx-auto px-4 sm:px-8">
       <div className="flex justify-between items-center mb-8">
-        <h2 className="text-2xl font-semibold leading-tight uppercase">
-          sobre las ventas
-        </h2>
+        <h2 className="text-2xl font-semibold leading-tight uppercase">Sobre las ventas</h2>
         <div className="flex items-center">
           <button
-            className="p-2 mr-2 bg-gray-400 rounded flex items-center
-                    font-medium text-white uppercase
-                    focus:outline-none hover:bg-gray-700 hover:shadow-none"
+            className="p-2 mr-2 bg-gray-400 rounded flex items-center font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
           >
             <PlusIcon size={16} className="mr-1" />
             Venta
           </button>
           <button
-            onClick={handleFilter}
-            className="p-2 bg-gray-400 rounded flex items-center
-                    font-medium text-white uppercase
-                    focus:outline-none hover:bg-gray-700 hover:shadow-none"
+            onClick={handleFilterToggle}
+            className="p-2 bg-gray-400 rounded flex items-center font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
           >
             <CalendarIcon size={16} className="mr-1" />
-            {filter}
+            {filter === "monthly" ? "mensual" : "semanal"}
           </button>
         </div>
       </div>
@@ -89,117 +111,35 @@ export default function SalesSection() {
             <table className="min-w-full leading-normal">
               <thead>
                 <tr>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Marca
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Descripción
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Fecha
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Vendedor
-                  </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Monto
-                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Marca</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Descripción</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Fecha</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Vendedor</th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Monto</th>
                 </tr>
               </thead>
-              <tbody>
-                {sales?.monthly.map((sale, index) => (
-                  <tr key={index}>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-600 whitespace-no-wrap">
-                        {sale.brand}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-600 whitespace-no-wrap">
-                        {sale.description}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-600 whitespace-no-wrap">
-                        {formatDate(sale.sellDate.toString())}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-600 whitespace-no-wrap">
-                        {sale.user.name}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                      <p className="text-gray-600 whitespace-no-wrap">
-                        {sale.amount}
-                      </p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
+              <tbody>{renderTableRows(sales?.monthly)}</tbody>
             </table>
           </div>
         </div>
       )}
+
       {filter === "weekly" &&
         sales?.weekly.map((week, index) => (
-          <div
-            key={index}
-            className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto"
-          >
+          <div key={index} className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <p className="text-gray-600 mb-1">Semana n°: {week.week}</p>
             <div className="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
               <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Marca
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Descripción
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Fecha
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Vendedor
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Monto
-                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Marca</th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Descripción</th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Fecha</th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Vendedor</th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">Monto</th>
                   </tr>
                 </thead>
-                <tbody>
-                  {week.items.map((sale, windex) => (
-                    <tr key={windex}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-600 whitespace-no-wrap">
-                          {sale.brand}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-600 whitespace-no-wrap">
-                          {sale.description}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-600 whitespace-no-wrap">
-                          {formatDate(sale.sellDate.toString())}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-600 whitespace-no-wrap">
-                          {sale.user.name}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                        <p className="text-gray-600 whitespace-no-wrap">
-                          {sale.amount}
-                        </p>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
+                <tbody>{renderTableRows(week.items)}</tbody>
               </table>
             </div>
           </div>
--- app/login/components/FormLogin.tsx
+++ app/login/components/FormLogin.tsx
@@ -1,26 +1,71 @@
-'use client';
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export const FormLogin = () => {
+  const [loginError, setLoginError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    setLoading(true);
+    try {
+      const response = await fetch('https://cloth-seller.vercel.app/api/v1/auth/sign-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        setLoginError(true);
+        reset();
+        setTimeout(() => {
+          setLoginError(false);
+        }, 5000);
+      }
+
+      const result = await response.json();
+      localStorage.setItem('token', JSON.stringify(result.accessToken));
+      reset();
+      router.push('/sales');
+    } catch (error) {
+      console.log(error.message)
+    }
+    setLoading(false);
+  };
+
   return (
-    <form className="mt-10" method="POST">
+    <form className="mt-10" method="POST" onSubmit={handleSubmit(onSubmit)}>
       <input
-        id="email"
+        {...register("email", { required: true })}
         type="email"
-        name="email"
         placeholder="Ingrese su correo"
         autoComplete="email"
-        required
         className="block w-full p-3 my-3 text-gray-800 border border-gray-100 focus:text-gray-500 focus:border-gray-200"
       />
       <input
-        id="password"
+        {...register("password", { required: true })}
         type="password"
-        name="password"
         placeholder="Ingrese su clave"
-        required
         className="block w-full p-3 my-3 text-gray-800 border border-gray-100 focus:text-gray-500 focus:border-gray-200"
       />
+      {(errors.email || errors.password) && (
+        <span className="text-red-500 text-xs">Necesitas ingresar tus credenciales</span>
+      )}
+      {loginError && <span className="text-red-500 text-xs">Error en la autenticación, ingresa de nuevo tus credenciales</span>}
 
       <button
         type="submit"
@@ -28,7 +73,7 @@ export const FormLogin = () => {
                     font-medium text-white uppercase
                     focus:outline-none hover:bg-gray-700 hover:shadow-none"
       >
-        Ingresar
+        {loading ? 'Cargando...' : 'Ingresar'}
       </button>
     </form>
   );
